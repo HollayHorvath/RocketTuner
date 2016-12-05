15f7e72315d5c0f6fe305bbf75d0bb90e6c752b3
Zsombor Hollay-Horváth <hollay.horvath@gmail.com>
2016-12-05 20:12:07 +0000

    GUI rewritten with Glade

--- rockettuner.py
+++ rockettuner.py
@@ -19,334 +19,193 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
 from gi.repository import Gtk, Gdk, Gst
 
-class GrafiteWindow(Gtk.Window):
+class RocketTuner:
+
+    ''' TODO: named tuple? '''
+    octaveRanges = (0, 9)
+
+    defaultFreq = 440.0
+
+    octaves = tuple(range(octaveRanges[0], octaveRanges[1]))
+    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C+']
+
+    scales = {
+        'Equal Temperament':
+            tuple((2 ** (1/12)) ** x
+                for x in range(12)),
+        'Werckmeister I (III)': (
+            1.0000000000000000, # C,  1/1
+            1.0534979423868314, # C#, 256/243
+            1.1174033085417048, # D,  64/81 * 2**(1/2)
+            1.1851851851851851, # D#, 32/27
+            1.2528272487271466, # E,  256/243 * 2**(1/4)
+            1.3333333333333333, # F,  4/3
+            1.4046639231824416, # F#, 1024/729
+            1.4949269604510480, # G,  8/9 * 8**(1/4)
+            1.5802469135802468, # G#, 128/81
+            1.6704363316361952, # A,  1024/729 * 2**(1/4)
+            1.7777777777777777, # A#, 16/9
+            1.8792408730907195  # B,  128/81 * 2**(1/4)
+        ),
+        'Werckmeister II (IV)': (
+            1.0000000000000000, # C,  1/1
+            1.0487500117602806, # C#, 16384/19683 * 2**(1/3)
+            1.1199298221287761, # D,  8/9 * 2**(1/3)
+            1.1851851851851851, # D#, 32/27
+            1.2542428064933920, # E,  64/81 * 4**(1/3)
+            1.3333333333333333, # F,  4/3
+            1.4046639231824416, # F#, 1024/729
+            1.4932397628383682, # G,  32/27 * 2**(1/3)
+            1.5731250176404208, # G#, 8192/6561 * 2**(1/3)
+            1.6723237419911896, # A,  256/243 * 4**(1/3)
+            1.7858261834642244, # A#, 9/(4 * 2**(1/3))
+            1.8728852309099222  # B,  4096/2187
+        ),
+        'Werckmeister III (V)': (
+            1.0000000000000000, # C,  1/1
+            1.0570729911135297, # C#, 8/9*2**(1/4)
+            1.1250000000000000, # D,  9/8
+            1.1892071150027210, # D#, 2**(1/4)
+            1.2570787221094177, # E,  8/9*2**(1/2)
+            1.3378580043780612, # F,  9/8*2**(1/4)
+            1.4142135623730951, # F#  2**(1/2)
+            1.5000000000000000, # G,  3/2
+            1.5802469135802468, # G#, 128/81
+            1.6817928305074290, # A,  8**(1/4)
+            1.7838106725040817, # A#, 3/(8**(1/4))
+            1.8856180831641267  # B,  4/3*2**(1/2)
+        ),
+        'Werckmeister IV (VI)': (
+            1.0000000000000000, # C,  1/1
+            1.0537634408602150, # C#, 98/93
+            1.1136363636363635, # D,  49/44
+            1.1878787878787880, # D#, 196/165
+            1.2564102564102564, # E,  49/39
+            1.3333333333333333, # F,  4/3
+            1.4100719424460430, # F#, 196/139
+            1.4961832061068703, # G,  196/131
+            1.5806451612903225, # G#, 49/31
+            1.6752136752136753, # A,  196/117
+            1.7818181818181817, # A#, 98/55
+            1.8846153846153846  # B,  49/26
+        )
+    }
+
+    @classmethod
+    def noteDiff(cls, tuning, audible):
+        ''' TODO: named tuple? '''
+        return (audible[0] - tuning[0],
+                cls.notes.index(audible[1]) - cls.notes.index(tuning[1]))
+
     def __init__(self):
-        Gtk.Window.__init__(self, title='RocketTuner')
-        self.set_icon_from_file('icon/icon64.png')
-# Extended list - C+ added for complete cycle
-        ''' Main values '''
-        self.notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C+']
-        self.octaves = list(range(9))
+        ''' Glade/Gtk/Gst init'''
+        self.gladefile = 'rockettuner.glade'
+        self.builder = Gtk.Builder()
 
-        self.tuningFreq = 440
-        self.tuningNote = 'A'
-        self.tuningOctave = 4
+        self.builder.add_from_file(self.gladefile)
+        self.builder.connect_signals(self)
 
-        self.audibleFreq = 440
-        self.audibleNote = 'A'
-        self.audibleOctave = 4
+        self.window = self.builder.get_object('rockettuner')
+        self.window.set_icon_from_file('icon/icon64.png')
 
-        self.root = 2 ** (1 / 12)
+        self.tuningFreqObj = self.builder.get_object('entry_tuningFreq')
 
-        self.volume = 1.0
+        self.tuningNoteObj = self.builder.get_object('combo_tuningNote')
+        self.tuningOctaveObj = self.builder.get_object('combo_tuningOctave')
 
-        self.buttonTrigger = False
-        self.buttonTimeout = None
+        self.audibleFreqObj = self.builder.get_object('label_audibleFreq')
 
-        ''' Set window values '''
-        self.set_position(Gtk.WindowPosition.CENTER)
-        self.set_border_width(10)
+        self.audibleNoteObj = self.builder.get_object('combo_audibleNote')
+        self.audibleOctaveObj = self.builder.get_object('combo_audibleOctave')
 
-        self.connect('delete-event', Gtk.main_quit)
+        self.tuningScaleObj = self.builder.get_object('combo_tuningScale')
 
-        grid = Gtk.Grid(column_spacing=10, row_spacing=6)
-        self.add(grid)
+        self.tuningFreq = self.defaultFreq
+        self.audibleFreq = self.audibleFreq
 
-        ''' Tuning freq '''
-        labelTuningFreq = Gtk.Label('Tuning frequency')
+        Gst.init_check()
 
-        self.entryTuningFreq = Gtk.Entry()
-        self.entryTuningFreq.set_text('%0.2f'%self.tuningFreq)
-
-        labelEntryTuningFreq = Gtk.Label('Hz')
-
-        buttonTuningFreqUp = Gtk.Button()
-        buttonTuningFreqUp.set_label('+')
-
-        buttonTuningFreqDown = Gtk.Button()
-        buttonTuningFreqDown.set_label('-')
-
-        # Callbacks
-        self.entryTuningFreq.connect('changed', self.changedTuningFreq)
-        buttonTuningFreqUp.connect('clicked', self.changedTuningFreq)
-        buttonTuningFreqDown.connect('clicked', self.changedTuningFreq)
-
-        # Grid
-        grid.attach(labelTuningFreq, 0, 0, 6, 1)
-        grid.attach(self.entryTuningFreq, 0, 1, 3, 1)
-        grid.attach(labelEntryTuningFreq, 3, 1, 1, 1)
-
-        grid.attach(buttonTuningFreqUp, 4, 1, 1, 1)
-        grid.attach(buttonTuningFreqDown, 5, 1, 1, 1)
-
-        ''' Tuning note/octave '''
-        labelTuningNote = Gtk.Label('Tuning note')
-
-        storeTuningNote = Gtk.ListStore(str)
-        for note in self.notes:
-            storeTuningNote.append([note])
-
-        rendererTuningNote = Gtk.CellRendererText()
-
-        self.comboTuningNote = Gtk.ComboBox.new_with_model(storeTuningNote)
-        self.comboTuningNote.pack_start(rendererTuningNote, True)
-        self.comboTuningNote.add_attribute(rendererTuningNote, 'text', 0)
-        self.comboTuningNote.set_active(9)
-
-        buttonTuningNoteUp = Gtk.Button()
-        buttonTuningNoteUp.set_label('+')
-
-        buttonTuningNoteDown = Gtk.Button()
-        buttonTuningNoteDown.set_label('-')
-
-        labelTuningOctave = Gtk.Label('Tuning octave')
-
-        storeTuningOctave = Gtk.ListStore(int)
-        for octave in self.octaves:
-            storeTuningOctave.append([octave])
-
-        rendererTuningOctave = Gtk.CellRendererText()
-
-        self.comboTuningOctave = Gtk.ComboBox.new_with_model(storeTuningOctave)
-        self.comboTuningOctave.pack_start(rendererTuningOctave, True)
-        self.comboTuningOctave.add_attribute(rendererTuningOctave, 'text', 0)
-        self.comboTuningOctave.set_active(4)
-
-        buttonTuningOctaveUp = Gtk.Button()
-        buttonTuningOctaveUp.set_label('+')
-
-        buttonTuningOctaveDown = Gtk.Button()
-        buttonTuningOctaveDown.set_label('-')
-
-        # Callbacks
-        self.comboTuningNote.connect('scroll-event', self.scrolledTuningNote)
-        self.comboTuningNote.connect('changed', self.changedTuningNote)
-        buttonTuningNoteUp.connect('clicked', self.changedTuningNote)
-        buttonTuningNoteDown.connect('clicked', self.changedTuningNote)
-
-        self.comboTuningOctave.connect('changed', self.changedTuningOctave)
-        buttonTuningOctaveUp.connect('clicked', self.changedTuningOctave)
-        buttonTuningOctaveDown.connect('clicked', self.changedTuningOctave)
-
-        # Grid
-        grid.attach(labelTuningNote, 0, 3, 3, 1)
-        grid.attach(self.comboTuningNote, 0, 4, 1, 1)
-
-        grid.attach(buttonTuningNoteUp, 1, 4, 1, 1)
-        grid.attach(buttonTuningNoteDown, 2, 4, 1, 1)
-
-        grid.attach(labelTuningOctave, 3, 3, 3, 1)
-        grid.attach(self.comboTuningOctave, 3, 4, 1, 1)
-
-        grid.attach(buttonTuningOctaveUp, 4, 4, 1, 1)
-        grid.attach(buttonTuningOctaveDown, 5, 4, 1, 1)
-
-        ''' Audible note/octave '''
-        labelAudibleNote = Gtk.Label('Audible note')
-
-        storeAudibleNote = Gtk.ListStore(str)
-        for note in self.notes:
-            storeAudibleNote.append([note])
-
-        rendererAudibleNote = Gtk.CellRendererText()
-        self.comboAudibleNote = Gtk.ComboBox.new_with_model(storeAudibleNote)
-        self.comboAudibleNote.pack_start(rendererAudibleNote, True)
-        self.comboAudibleNote.add_attribute(rendererAudibleNote, 'text', 0)
-        self.comboAudibleNote.set_active(9)
-
-        buttonAudibleNoteUp = Gtk.Button()
-        buttonAudibleNoteUp.set_label('+')
-
-        buttonAudibleNoteDown = Gtk.Button()
-        buttonAudibleNoteDown.set_label('-')
-
-        labelAudibleOctave = Gtk.Label('Audible octave')
-
-        storeAudibleOctave = Gtk.ListStore(int)
-        for octave in self.octaves:
-            storeAudibleOctave.append([octave])
-
-        rendererAudibleOctave = Gtk.CellRendererText()
-        self.comboAudibleOctave = Gtk.ComboBox.new_with_model(storeAudibleOctave)
-        self.comboAudibleOctave.pack_start(rendererAudibleOctave, True)
-        self.comboAudibleOctave.add_attribute(rendererAudibleOctave, 'text', 0)
-        self.comboAudibleOctave.set_active(4)
-
-        buttonAudibleOctaveUp = Gtk.Button()
-        buttonAudibleOctaveUp.set_label('+')
-
-        buttonAudibleOctaveDown = Gtk.Button()
-        buttonAudibleOctaveDown.set_label('-')
-
-        # Callbacks
-        self.comboAudibleNote.connect('scroll-event', self.scrolledAudibleNote)
-        self.comboAudibleNote.connect('changed', self.changedAudibleNote)
-        buttonAudibleNoteUp.connect('clicked', self.changedAudibleNote)
-        buttonAudibleNoteDown.connect('clicked', self.changedAudibleNote)
-
-        self.comboAudibleOctave.connect('changed', self.changedAudibleOctave)
-        buttonAudibleOctaveUp.connect('clicked', self.changedAudibleOctave)
-        buttonAudibleOctaveDown.connect('clicked', self.changedAudibleOctave)
-
-        # Grid
-        grid.attach(labelAudibleNote, 0, 5, 3, 1)
-        grid.attach(self.comboAudibleNote, 0, 6, 1, 1)
-
-        grid.attach(buttonAudibleNoteUp, 1, 6, 1, 1)
-        grid.attach(buttonAudibleNoteDown, 2, 6, 1, 1)
-
-        grid.attach(labelAudibleOctave, 3, 5, 3, 1)
-        grid.attach(self.comboAudibleOctave, 3, 6, 1, 1)
-
-        grid.attach(buttonAudibleOctaveUp, 4, 6, 1, 1)
-        grid.attach(buttonAudibleOctaveDown, 5, 6, 1, 1)
-
-        ''' Audible freq, misc '''
-        self.labelAudibleFreq = Gtk.Label('Audible frequency: %0.2f Hz'%self.audibleFreq)
-
-        buttonVolume = Gtk.VolumeButton()
-        buttonVolume.set_value(self.volume)
-
-        buttonPlay = Gtk.ToggleButton('▶')
-
-        # Callbacks
-        buttonVolume.connect('value-changed', self.changeVolume)
-        buttonPlay.connect('clicked', self.clickPlay)
-
-        # Grid
-        grid.attach(self.labelAudibleFreq, 0, 7, 3, 1)
-
-        grid.attach(buttonVolume, 3, 7, 2, 1)
-        grid.attach(buttonPlay, 5, 7, 1, 1)
-
-        ''' Audio settings '''
-        self.audio = Gst.Pipeline(name='note')
-        self.source = Gst.ElementFactory.make('audiotestsrc', 'src')
-        sink = Gst.ElementFactory.make('autoaudiosink', 'output')
-
-        self.audio.add(self.source)
-        self.audio.add(sink)
-        self.source.link(sink)
-
-    def noteDiff(self):
-        return (self.audibleOctave-self.tuningOctave) * 12 + self.notes.index(self.audibleNote) - self.notes.index(self.tuningNote)
-
-    def calcAudibleFreq(self):
-        self.audibleFreq = self.tuningFreq*(self.root**self.noteDiff())
-
-    def play(self):
-        self.audio.set_state(Gst.State.PLAYING)
-
-    def stop(self):
-        self.audio.set_state(Gst.State.NULL)
-
-    def changeVolume(self, obj, volume):
-        self.volume = volume
-        #TODO: how to change volume?
-        #~self.audio.set_state(Gst.State.NULL)
-        #~self.audio.volume = self.volume
-        #~self.audio.set_state(Gst.State.PLAYING)
-        pass
-
-    def clickPlay(self, obj):
-        if obj.get_active():
-            self.play()
-        else:
-            self.stop()
-
-    def changeFreq(self):
-        self.calcAudibleFreq()
-        self.labelAudibleFreq.set_label('Audible frequency: %0.2f Hz'%self.audibleFreq)
-        self.source.set_property('freq', self.audibleFreq)
-
-    def changedTuningFreq(self, obj):
+    @property
+    def tuningFreq(self):
         try:
-            tuningFreq = float(self.entryTuningFreq.get_text())
-            if type(obj) == Gtk.Button:
-                label = obj.get_label()
-                if label == '+':
-                    tuningFreq += 1
-                elif label == '-':
-                    tuningFreq -= 1
-                self.entryTuningFreq.set_text(str(tuningFreq))
-            self.tuningFreq = tuningFreq
-            self.changeFreq()
-        except:
-            pass
+            return float(self.tuningFreqObj.get_text())
+        except ValueError:
+            return 'N/A'
 
-    def changedTuningNote(self, obj):
-        #!!! TODO add plus-minus
-        self.tuningNote = self.notes[self.comboTuningNote.get_active()]
-        self.tuningOctave = self.comboTuningOctave.get_active()
+    @tuningFreq.setter
+    def tuningFreq(self, value):
+        self.tuningFreqObj.set_text(str(value))
 
-        self.changeFreq()
+    @property
+    def audibleFreq(self):
+        tuningFreq = self.tuningFreq
 
-    def changedTuningOctave(self, obj):
-        #!!! TODO add plus-minus
-        self.tuningOctave = self.comboTuningOctave.get_active()
+        if type(tuningFreq) != float:
+            return 'N/A'
 
-        self.changeFreq()
+        tuning = (self.tuningOctave, self.tuningNote)
+        audible = (self.audibleOctave, self.tuningNote)
 
-    def scrolledTuningNote(self, obj, val):
-        if val.direction == Gdk.ScrollDirection.UP:
-            self.changeTuning(-1)
-        elif val.direction == Gdk.ScrollDirection.DOWN:
-            self.changeTuning(1)
+        octaves, notes = self.noteDiff(tuning, audible)
 
-    def changeTuning(self, direction):
-        note = self.comboTuningNote.get_active()
-        octave = self.comboTuningOctave.get_active()
+        return tuningFreq * (2**octaves) * self.scales[self.tuningScale][notes]
 
-        diff = note + direction
-        if diff < 0 and octave > 0:
-            note = 11
-            octave -= 1
-        elif diff > 11 and octave < 8:
-            note = 0
-            octave += 1
+    @audibleFreq.setter
+    def audibleFreq(self, value):
+        try:
+            audibleFreq = '%.2f'%value
+        except SyntaxError:
+            audibleFreq = 'N/A'
 
-        self.comboTuningNote.set_active(note)
-        self.comboTuningOctave.set_active(octave)
+        self.audibleFreqObj.set_text('Audible frequency: s% Hz'%audibleFreq)
 
-    def changedAudibleNote(self, obj):
-        #!!! TODO add plus-minus
-        self.audibleNote = self.notes[self.comboAudibleNote.get_active()]
-        self.audibleOctave = self.comboAudibleOctave.get_active()
+    @property
+    def tuningNote(self):
+        return self.notes[self.tuningNoteObj.get_active()]
 
-        self.changeFreq()
+    @tuningNote.setter
+    def tuningNote(self, value):
+        self.tuningNoteObj.set_active(self.notes.index(value))
 
-    def changedAudibleOctave(self, obj):
-        #!!! TODO add plus-minus
-        self.audibleOctave = self.comboAudibleOctave.get_active()
+    @property
+    def tuningOctave(self):
+        return self.tuningOctaveObj.get_active()
 
-        self.changeFreq()
+    @tuningOctave.setter
+    def tuningOctave(self, value):
+        self.tuningOctaveObj.set_active(value)
 
-    def scrolledAudibleNote(self, obj, val):
-        if val.direction == Gdk.ScrollDirection.UP:
-            self.changeAudible(-1)
-        elif val.direction == Gdk.ScrollDirection.DOWN:
-            self.changeAudible(1)
+    @property
+    def audibleNote(self):
+        return self.notes[self.audibleNoteObj.get_active()]
 
-    def changeAudible(self, direction):
-        note = self.comboAudibleNote.get_active()
-        octave = self.comboAudibleOctave.get_active()
+    @audibleNote.setter
+    def audibleNote(self, value):
+        self.audibleNoteObj.set_active(self.notes.index(value))
 
-        diff = note + direction
-        if diff < 0 and octave > 0:
-            note = 11
-            octave -= 1
-        elif diff > 11 and octave < 8:
-            note = 0
-            octave += 1
+    @property
+    def audibleOctave(self):
+        return self.audibleOctaveObj.get_active()
 
-        self.comboAudibleNote.set_active(note)
-        self.comboAudibleOctave.set_active(octave)
+    @audibleOctave.setter
+    def audibleOctave(self, value):
+        self.audibleOctaveObj.set_active(value)
+
+    @property
+    def tuningScale(self):
+        print(dir(self.tuningScaleObj))
+        print(self.tuningScaleObj.get_data)
+        return self.tuningScaleObj.get_active()
+
+    def onDeleteWindow(self, obj, data):
+        Gtk.main_quit()
+
+    def show_all(self):
+        self.window.show()
 
 if __name__ == '__main__':
 
-    Gst.init_check()
-
-    window = GrafiteWindow()
+    window = RocketTuner()
     window.show_all()
 
     Gtk.main()
